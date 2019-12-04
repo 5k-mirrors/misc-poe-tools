3cf43e4ad3afad352e035b5c6df62eb1bf68669c
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-12-05 00:58:52 +0100

    Export leagues

--- item-price-compare/src/components/app.js
+++ item-price-compare/src/components/app.js
@@ -1,46 +1,7 @@
 import React from "react";
 import {fetchJSON} from '../functions/http'
-import {leaguesApi, itemsApi} from '../functions/config'
-
-const mapToMetaLeagues = leagues => {
-  const tempLeagues = leagues.filter(leagueHash => {
-    const leagueName = leagueHash["id"];
-    return !(["ssf", "standard"].some(prohibitedWord =>
-      leagueName.toLowerCase().includes(prohibitedWord)
-    ) || leagueName.toLowerCase() === "hardcore");
-  });
-
-  const tempSC = tempLeagues.filter(leagueHash => {
-    return !leagueHash["id"].toLowerCase().includes("hardcore");
-  });
-
-  const tempHC = tempLeagues.filter(leagueHash => {
-    return leagueHash["id"].toLowerCase().includes("hardcore");
-  });
-
-  if(tempSC.length !== 1 || tempHC.length !== 1) {
-    throw new Error(`Couldn't indentify temp leagues in ${JSON.stringify(tempLeagues)}`);
-  }
-
-  return {
-    "Temp SC": tempSC[0]["id"],
-    "Temp HC": tempHC[0]["id"],
-    "Standard": "Standard",
-    "Hardcore": "Hardcore",
-  };
-};
-
-const leagues = () => {
-  return ["Temp SC", "Temp HC", "Standard", "Hardcore"];
-};
-
-const fetchLeagues = () => {
-  return fetchJSON(leaguesApi()).then((leagues) => {
-    return mapToMetaLeagues(leagues);
-  }).catch((error) => {
-    console.error(`Couldn't fetch leagues: ${error}`);
-  });
-};
+import {itemsApi} from '../functions/config'
+import {leagues, fetchLeagues} from '../functions/leagues'
 
 class App extends React.Component {
   constructor(props) {
