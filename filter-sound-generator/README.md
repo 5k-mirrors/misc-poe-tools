12038f532578b73d56ae4acac60e829bfa4ec901
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-12-12 00:43:22 +0100

    Print filter entries, add more customization to filter entries, add mroe filter entries

--- filter-sound-generator/README.md
+++ filter-sound-generator/README.md
@@ -2,19 +2,28 @@
 
 Create google project, enable TTS, get key, save it in the folder as `credentials.json`.
 
-`npm run start`
+```sh
+npm run start
+cp output/*.mp3 ...
+```
 
-To the filter add something like
+The generated sounds are low volume compared to the maximum setting (300) of the default sounds. The maximum is often used. Lower these sounds in the filter by e.g. search & replacing them in VSCode:
+- `PlayAlertSound ([0-9]*) 300` -> `PlayAlertSound $1 100`
+- `PlayAlertSoundPositional ([0-9]*) 300` -> `PlayAlertSoundPositional $1 100`
+
+And don't forget to compensate for this in the sound settings.
+
+The code to be added to the filter will be printed on screen. E.g.
 
 ```
 Show
-	SocketGroup BBB
-	ItemLevel <= 12
-	SetFontSize 45
-	SetTextColor 255 0 0 255                # TEXTCOLOR:	 T0 Item
-	SetBorderColor 255 0 0 255              # BORDERCOLOR:	 T0 Item
-	SetBackgroundColor 255 255 255 255      # BACKGROUND:	 T0 Drop
-	PlayEffect Red
-	MinimapIcon 0 Red Star
+SocketGroup BBB
+ItemLevel <= 12
+SetFontSize 45
+SetTextColor 255 0 0 255                # TEXTCOLOR:	 T0 Item
+SetBorderColor 255 0 0 255              # BORDERCOLOR:	 T0 Item
+SetBackgroundColor 255 255 255 255      # BACKGROUND:	 T0 Drop
+PlayEffect Red
+MinimapIcon 0 Red Star
 CustomAlertSound "bbb.mp3"
 ```
--- filter-sound-generator/index.js
+++ filter-sound-generator/index.js
@@ -4,31 +4,131 @@ const util = require('util');
 
 const client = new textToSpeech.TextToSpeechClient();
 
-const texts = [
-  'bbb',
-  'bbg',
-  'bgg',
-  '4 link b',
-  '4 link g',
-  '4 link bg',
-  '5 link bg',
-];
-const speed = 2;
+const defaultStyles = `SetFontSize 45
+SetTextColor 255 0 0 255                # TEXTCOLOR:	 T0 Item
+SetBorderColor 255 0 0 255              # BORDERCOLOR:	 T0 Item
+SetBackgroundColor 255 255 255 255      # BACKGROUND:	 T0 Drop
+PlayEffect Red
+MinimapIcon 0 Red Star`
 
-texts.forEach((text) => {
-  const outputFile = `output/${text}.mp3`
+const speed = 2;
+const volume = 1;
+
+const customFilters = [
+  {
+    name: 'wisdom',
+    filterOptions: 'BaseType "Scroll of Wisdom"',
+  },
+  {
+    name: 'portal',
+    filterOptions: 'BaseType "Portal Scroll"',
+  },
+  {
+    name: 'augment',
+    filterOptions: 'BaseType "Orb of Augmentation"',
+  },
+  {
+    name: 'wisdoms',
+    filterOptions: 'BaseType "Blacksmith\'s Whetstone"',
+  },
+  {
+    name: 'wisdoms',
+    filterOptions: 'BaseType "Armourer\'s Scrap"',
+  },
+  {
+    name: 'topaz',
+    filterOptions: 'BaseType "Topaz Ring"',
+  },
+  {
+    name: 'sapphire',
+    filterOptions: 'BaseType "Sapphire Ring"',
+  },
+  {
+    name: 'transmute',
+    filterOptions: 'BaseType "Orb of Transmutation"',
+  },
+  {
+    name: 'alt',
+    filterOptions: 'BaseType "Orb of Alteration"',
+  },
+  {
+    name: 'chance',
+    filterOptions: 'BaseType "Orb of Chance"',
+  },
+  {
+    name: 'alch',
+    filterOptions: 'BaseType "Orb of Alchemy"',
+  },
+  {
+    name: 'bbb',
+    filterOptions: 'SocketGroup BBB\nItemLevel <= 14\n',
+  },
+  {
+    name: 'bbg',
+    filterOptions: 'SocketGroup BBG\nItemLevel <= 14\n',
+  },
+  {
+    name: 'bgg',
+    filterOptions: 'SocketGroup BGG\nItemLevel <= 14',
+  },
+  {
+    name: '4 link b',
+    filterOptions: 'LinkedSockets 4\nSocketGroup BBBB',
+  },
+  {
+    name: '4 link g',
+    filterOptions: 'LinkedSockets 4\nSocketGroup GGGG',
+  },
+  {
+    name: '4 link bg',
+    filterOptions: 'LinkedSockets 4\nSocketGroup BBGG',
+  },
+  {
+    name: '4 link bg',
+    filterOptions: 'LinkedSockets 4\nSocketGroup BBBG',
+  },
+  {
+    name: '4 link bg',
+    filterOptions: 'LinkedSockets 4\nSocketGroup BGGG',
+  },
+  {
+    name: 'quicksilver',
+    filterOptions: 'BaseType "Quicksilver Flask"',
+  },
+];
+
+let filterOutput = "";
+
+const createFilterStyle = (soundFileName, filterOptions) => {
+  return `Show
+${filterOptions}
+${defaultStyles}
+CustomAlertSound "${soundFileName}"
+
+`;
+}
+
+customFilters.forEach((customFilter) => {
+  const outputFileName = `${customFilter['name']}.mp3`
+  const outputFilePath = `output/${outputFileName}`
 
   const request = {
-    input: {text: text},
+    input: {text: customFilter['name']},
     voice: {languageCode: 'en-US', ssmlGender: 'FEMALE'},
-    audioConfig: {audioEncoding: 'MP3', speakingRate: speed},
+    audioConfig: {audioEncoding: 'MP3', speakingRate: speed, volumeGainDb: volume},
   };
+
+  filterOutput += createFilterStyle(outputFileName, customFilter['filterOptions']);
+
   client.synthesizeSpeech(request).then(([response]) => {
     const writeFile = util.promisify(fs.writeFile);
-    return writeFile(outputFile, response.audioContent, 'binary');
+    return writeFile(outputFilePath, response.audioContent, 'binary');
   }).then(() => {
-    console.log(`Audio content written to file: ${outputFile}`);
+    console.log(`Audio content written to file: ${outputFilePath}`);
   }).catch((e) => {
     console.error(e);
   });
 });
+
+console.log("Add this to your filter:\n");
+console.log(filterOutput);
