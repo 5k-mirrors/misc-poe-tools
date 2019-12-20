e8b566fcd3e521b77fc10e677142f72948b71d9e
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-12-20 01:57:35 +0100

    Make poc work with real data

--- what-to-flip/src/components/app.js
+++ what-to-flip/src/components/app.js
@@ -27,7 +27,7 @@ export default () => {
 
   useEffect(() => {
     setSelectedLeague(leagues[selectedMetaLeague]);
-  }, [leagues]);
+  }, [leagues, selectedMetaLeague]);
 
   return (
     <div>
@@ -35,7 +35,7 @@ export default () => {
       <select
         name="league"
         value={selectedMetaLeague}
-        onChange={event => leagueSelected(event)}
+        onChange={event => setSelectedMetaLeague(event.target.value)}
       >
         {metaLeagues().map(league => (
           <option key={league} value={league}>
@@ -51,7 +51,7 @@ export default () => {
             {comparisonsByGroup[group].map((comparison, i) => (
               <li key={i}>
                 <label>
-                  {/* {comparisonText(comparison)} */}
+                  {comparisonText(comparison, selectedLeague, items)}
                   </label>
               </li>
             ))}
@@ -62,28 +62,32 @@ export default () => {
   );
 };
 
-const comparisonText = (comparison) => {
+const comparisonText = (comparison, selectedLeague, items) => {
   let text = `${comparison.name}: `;
 
   if (
-    !this.state.selectedLeague ||
-    !this.state.items ||
-    !this.state.items[this.state.selectedLeague]
+    !selectedLeague ||
+    !items ||
+    !items[selectedLeague]
   ) {
     text += "?";
     return text;
   }
 
-  text += `${this.compareText(
+  text += `${compareText(
     comparison.base,
-    comparison.compare
+    comparison.compare,
+    items,
+    selectedLeague,
   )} chaos profit`;
 
   const pieces = comparison.compare.length;
 
   if (pieces !== 0)
-    text += `, cost: ${this.costText(
-      comparison.compare
+    text += `, cost: ${costText(
+      comparison.compare,
+      items,
+      selectedLeague,
     )} chaos, pieces: ${pieces}`;
 
   if (comparison.comment) {
@@ -93,25 +97,8 @@ const comparisonText = (comparison) => {
   return text;
 };
 
-// const updateItems = (selectedLeague, setItems) => {
-//   fetchItems(selectedLeague).then(items => {
-//     setItems(items);
-//   });
-// };
-
-const leagueSelected = (event) => {
-  // const selectedLeague = this.state.leagues[event.target.value];
-  // this.setState(
-  //   {
-  //     selectedLeague,
-  //     selectedMetaLeague: event.target.value,
-  //   },
-  //   () => this.updateItems()
-  // );
-}
-
-const costText  = (names) => {
-  const costText = cost(this.state.items, this.state.selectedLeague, names);
+const costText  = (names, items, selectedLeague) => {
+  const costText = cost(items, selectedLeague, names);
 
   if (!costText) {
     return "N/A";
@@ -119,10 +106,10 @@ const costText  = (names) => {
   return costText;
 }
 
-const compareText = (base, compare) => {
+const compareText = (base, compare, items, selectedLeague) => {
   const comparison = comparePrice(
-    this.state.items,
-    this.state.selectedLeague,
+    items,
+    selectedLeague,
     base,
     compare
   );
@@ -132,132 +119,3 @@ const compareText = (base, compare) => {
   }
   return comparison;
 }
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       comparisonsByGroup: comparisons(),
-//       selectedMetaLeague: "Temp SC",
-//     };
-//   }
-
-//   componentDidMount() {
-//     const { selectedMetaLeague } = this.state;
-
-//     fetchLeagues().then(leagues => {
-//       console.log(`Leagues: ${JSON.stringify(leagues)}`);
-//       this.setState(
-//         { leagues, selectedLeague: leagues[selectedMetaLeague] },
-//         () => this.updateItems()
-//       );
-//     });
-//   }
-
-//   render() {
-//     const { selectedMetaLeague, comparisonsByGroup } = this.state;
-
-//     return (
-//       <div>
-//         <h1>PoE - What to flip?</h1>
-//         <select
-//           name="league"
-//           value={selectedMetaLeague}
-//           onChange={event => this.leagueSelected(event)}
-//         >
-//           {metaLeagues().map(league => (
-//             <option key={league} value={league}>
-//               {league}
-//             </option>
-//           ))}
-//         </select>
-
-//         <ol>
-//           {Object.keys(comparisonsByGroup).map(group => (
-//             <div key={group}>
-//               <h2>{group}</h2>
-//               {comparisonsByGroup[group].map((comparison, i) => (
-//                 <li key={i}>
-//                   <label>{this.comparisonText(comparison)}</label>
-//                 </li>
-//               ))}
-//             </div>
-//           ))}
-//         </ol>
-//       </div>
-//     );
-//   }
-
-//   comparisonText(comparison) {
-//     let text = `${comparison.name}: `;
-
-//     if (
-//       !this.state.selectedLeague ||
-//       !this.state.items ||
-//       !this.state.items[this.state.selectedLeague]
-//     ) {
-//       text += "?";
-//       return text;
-//     }
-
-//     text += `${this.compareText(
-//       comparison.base,
-//       comparison.compare
-//     )} chaos profit`;
-
-//     const pieces = comparison.compare.length;
-
-//     if (pieces !== 0)
-//       text += `, cost: ${this.costText(
-//         comparison.compare
-//       )} chaos, pieces: ${pieces}`;
-
-//     if (comparison.comment) {
-//       text += ` (${comparison.comment})`;
-//     }
-
-//     return text;
-//   }
-
-//   updateItems() {
-//     fetchItems(this.state.selectedLeague).then(items => {
-//       this.setState({ items });
-//     });
-//   }
-
-//   leagueSelected(event) {
-//     const selectedLeague = this.state.leagues[event.target.value];
-//     this.setState(
-//       {
-//         selectedLeague,
-//         selectedMetaLeague: event.target.value,
-//       },
-//       () => this.updateItems()
-//     );
-//   }
-
-//   costText(names) {
-//     const costText = cost(this.state.items, this.state.selectedLeague, names);
-
-//     if (!costText) {
-//       return "N/A";
-//     }
-//     return costText;
-//   }
-
-//   compareText(base, compare) {
-//     const comparison = comparePrice(
-//       this.state.items,
-//       this.state.selectedLeague,
-//       base,
-//       compare
-//     );
-
-//     if (!comparison) {
-//       return "N/A";
-//     }
-//     return comparison;
-//   }
-// }
-
-// export default App;
