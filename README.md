0a4fd86a7924a96cc88a94894814d20b3012f817
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-01-28 00:44:51 +0100

    Move what-to-flip to https://github.com/5k-mirrors/poe-what-to-flip

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 Some misc tools that don't have their own project (yet).
 
-- [What to flip](what-to-flip)
 - [Level like a coward](level-like-a-coward)
 - [poe.ninja API](doc/poe-ninja-api.md)
--- what-to-flip/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/app";
-
-import "./styles.css";
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
